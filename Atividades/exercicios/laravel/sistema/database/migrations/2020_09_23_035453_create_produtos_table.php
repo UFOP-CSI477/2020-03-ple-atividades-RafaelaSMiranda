796385f230a9910fb33b7ddfd535f054b792a5da
RafaelaSMiranda <rafaela.silva.m@outlook.com>
2020-09-23 01:34:24 -0300

    Aula 20 - criação de migrations

---
+++ Atividades/exercicios/laravel/sistema/database/migrations/2020_09_23_035453_create_produtos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('um', 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
